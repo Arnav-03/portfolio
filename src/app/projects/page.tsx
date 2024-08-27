3143239e317b58b7e78b48a777e4edc72e15d8ed
Arnav-03 <arnavarora0003@gmail.com>
2024-08-28 00:37:20 +0530

    projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -14,25 +14,35 @@ function page() {
     {
       title: "Kashitokaru",
       link: "/projects/kashitokaru",
+      tags: ["React", "Websocket", "Gemini API", "MongoDb", "Express"],
       desc: "Kashitokaru is a dynamic chat application offering real-time messaging, file sharing, and AI-powered smart replies. It integrates Google Sign-In and ensures seamless communication with a modern, responsive interface.",
       image: kashi,
     },
     {
       title: "ShopKrow",
       link: "/projects/shopkrow",
+      tags: [
+        "React",
+        "Express",
+        "Tailwind CSS",
+        "MongoDb",
+        "Google Cloud Platform",
+      ],
       desc: "ShopKrow is an e-commerce platform built with React and Tailwind CSS, featuring Stripe integration for secure payments and dynamic product pages. It offers optimized performance through dynamic content delivery, providing a seamless shopping experience.",
       image: shop,
     },
     {
       title: "Fileophile",
       link: "/projects/kashitokaru",
-      desc: "Fileophile is a user-friendly platform designed for seamless file sharing and storage. With features like real-time sharing, cloud storage, and easy integration with Google authentication, Fileophile ensures that your files are always at your fingertips, making collaboration and file management effortless.",
+      tags: ["Next.js", "Firebase", "Tailwind CSS", "Google Cloud Platform"],
+      desc: "Fileophile is a user-friendly platform designed for seamless file sharing and storage. With features like real-time sharing, cloud storage, and easy integration with Google authentication, Fileophile ensures that your files are always at your fingertips.",
       image: fileophile,
     },
     ,
     {
       title: "PeerMeet",
       link: "/projects/kashitokaru",
+      tags: ["React","WebRTC", "Tailwind CSS", "Socket.io"],
       desc: "PeerMeet is a video calling platform for seamless one-to-one interactions. Using WebRTC for high-quality, real-time communication and built with React and Tailwind CSS, it provides a sleek, responsive interface with smooth performance.",
       image: peermeet,
     },
@@ -44,28 +54,37 @@ function page() {
       <div className="flex flex-wrap p-[10px] gap-4 items-center justify-center">
         {projects.map((project) => (
           <div
-            key={project.title}
-            className="flex flex-col border-[2px] w-[300px] h-[420px] rounded-3xl overflow-hidden border-[#e9e3d5]"
+            key={project?.title}
+            className="flex flex-col border-[2px] w-[300px] h-[480px] rounded-3xl overflow-hidden border-[#e9e3d5]"
           >
             <div>
               <img
                 className="h-[150px] w-auto object-cover"
-                src={project.image.src}
+                src={project?.image.src}
                 alt=""
               />
             </div>
-            <div className="text-[#e9e3d5] p-2 pb-0 text-2xl">{project.title}</div>
+            <div className="flex job2 p-2  flex-wrap gap-2">
+              {project?.tags.map((tag) => (
+                <div className="inline-block bg-gray-600  rounded-lg bg-muted px-3 py-1 text-sm">
+                  {tag}
+                </div>
+              ))}
+            </div>
+            <div className="text-[#e9e3d5] p-2 pb-0 text-2xl">
+              {project?.title}
+            </div>
             <div className="job2 text-[#bdbab5] p-2 pt-0 text-[14px] text-start flex-grow">
-              {project.desc}
+              {project?.desc}
             </div>
             <div className="w-full flex justify-end px-4 mt-auto">
-              <Link
-                href={project.link}
+              <a
+                href={project?.link}
                 className="bg-[#e9e3d5] job mx-5 mb-2 text-lg flex gap-1 items-center text-[#252525] w-fit p-1.5 rounded"
               >
                 View Website
                 <Image src={arrow} width={25} alt="card" />
-              </Link>
+              </a>
             </div>
           </div>
         ))}
